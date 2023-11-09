0b0e167dfec87a0cd31a612038128220447c5d90
Jonathan van der Steege <jonathan@jonakeys.nl>
2023-11-09 21:02:08 +0100

    Correct berekenen percentage

--- bsn-proef.lisp
+++ bsn-proef.lisp
@@ -1,12 +1,13 @@
+(defparameter *multiplies9* '(-1 2 3 4 5 6 7 8 9))
+(defparameter *multiplies8* '(-1 2 3 4 5 6 7 8))
+
 (defun is-bsn-valid (bsn)
   "Test if bsn is valid (with elfproef)"
-  (let* ((multiplies9 '(-1 2 3 4 5 6 7 8 9))
-		 (multiplies8 '(-1 2 3 4 5 6 7 8))
-		 (use-list multiplies9)
-		 (sum 0))
-	(when (and (>= bsn 10000000) (<= bsn 999999999))
+  (let ((use-list *multiplies9*)
+		(sum 0))
+	(when (and (>= bsn 10000000) (< bsn 1000000000))
 	  (if (< bsn 100000000)
-		  (setf use-list multiplies8))
+		  (setf use-list *multiplies8*))
 	  (dolist (i use-list sum)
 		(setf sum (+ sum (* i (mod bsn 10))))
 		(setf bsn (truncate bsn 10))))
@@ -17,10 +18,15 @@
 (defun find-valid-pairs ()
   "Find a valid bsn pair. I.e. a valid nine number and eight number
 where the last digit is dropped."
-  (let ((counter 0))
+  (let ((counter 0) (sum-valid 0) (total-iterations 0))
 	(do ((i 100000000 (incf i)))
 		((= i 1000000000))
-	  (if (is-bsn-valid i)
-		  (if (is-bsn-valid (truncate i 10))
-			  (incf counter))))
-	(format nil "~d pairs (~,3f%)" counter (* 100 (/ counter 899999999)))))
+	  (when (is-bsn-valid i)
+		(incf sum-valid)
+		(if (is-bsn-valid (truncate i 10))
+			(incf counter)))
+	  (incf total-iterations))
+	(format t "~d pairs (~,3f% of ~d)~%"
+			counter
+			(* 100 (/ counter sum-valid))
+			sum-valid)))
